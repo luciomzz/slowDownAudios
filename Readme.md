b59d51bb6ee293f6b69ed0070beb7274168623db
luciomzz <luciano.mazza3@gmail.com>
2024-07-25 14:16:33 +0200

    main and coments added

---
+++ Readme.md
@@ -0,0 +1,19 @@
+Hi!
+
+If you are here, you probably need to slow down some audios. You are in the right place.
+
+This is a compact script that uses pydub (that needs ffmpeg) and sounstretch, which is part of soundtouch.
+
+At the time of writing, you can install ffmpeg ('essentials' version recommended) from the website https://www.gyan.dev/ffmpeg/builds/
+You can download the executable of soundstretch from the website https://www.surina.net/soundtouch/download.html
+You can install the pydub library with pip.
+
+Make sure you add ffmpeg and soundstretch to your PATH.
+
+Versions: 
+ffmpeg version 7.0.1-essentials_build-www.gyan.dev
+SoundStretch v2.3.3
+pydub v0.25.1
+
+
+
--- slowDownAudio.py
+++ slowDownAudio.py
@@ -1,18 +1,17 @@
-import os
-import re
-
 from pydub import AudioSegment
+import numpy as np
 import subprocess
 
-def slow_down_and_pitch_stretch(file_path, slowdown_factor, output_path):
+def slow_down_and_pitch_stretch(input_path, slowdown_factor, output_path):
     # Load the audio using pydub
-    audio = AudioSegment.from_file(file_path)
+    audio = AudioSegment.from_file(input_path)
 
     slowed_audio = audio._spawn(audio.raw_data, overrides={
         "frame_rate": int(audio.frame_rate / slowdown_factor)
     }).set_frame_rate(audio.frame_rate)
 
     # Export the original audio to a temporary file in wav format
+    # This will create a temporary file that is updated each time you use the script
     temp_path = "temp_original_audio.wav"
     slowed_audio.export(temp_path, format="wav")
 
@@ -24,3 +23,11 @@ def slow_down_and_pitch_stretch(file_path, slowdown_factor, output_path):
 
     return output_path
 
+if __name__ == "__main__":
+    # Make sure you put the right paths here 
+    input_path = 'test.mp3'
+    output_path = 'testSlower.mp3'
+
+    # The higher the slower, I recommend not going above 1.2. For high values artifacts can appear.
+    slowdown_output = 1.07
+    slow_down_and_pitch_stretch(input_path, slowdown_output, output_path)
