f1aad4db58d894740b2b864ed26d33f0eb9ed981
luciomzz <luciano.mazza3@gmail.com>
2024-07-27 09:08:59 +0200

    Removed wav files, translated to mp3, works fine. Added tests with speedups.

--- slowDownAudio.py
+++ slowDownAudio.py
@@ -1,10 +1,13 @@
 from pydub import AudioSegment
 import numpy as np
 import subprocess
+import os 
 
 def slow_down_and_pitch_stretch(input_path, slowdown_factor, output_path):
     # Load the audio using pydub
     audio = AudioSegment.from_file(input_path)
+    
+    output_path_wav = output_path.replace(".mp3", ".wav")
 
     slowed_audio = audio._spawn(audio.raw_data, overrides={
         "frame_rate": int(audio.frame_rate / slowdown_factor)
@@ -19,15 +22,20 @@ def slow_down_and_pitch_stretch(input_path, slowdown_factor, output_path):
     pitch_shift_semitones = 12 * np.log2(slowdown_factor)
 
     # Use soundstretch for slowing down and pitch correction
-    subprocess.run(["soundstretch", temp_path, output_path, f"-tempo={0}", f"-pitch={pitch_shift_semitones}"])
+    subprocess.run(["soundstretch", temp_path, output_path_wav, f"-tempo={0}", f"-pitch={pitch_shift_semitones}"])
+
+    wav_audio = AudioSegment.from_wav(output_path_wav)
+    wav_audio.export(output_path, format="mp3")
+    os.remove(output_path_wav)
+    os.remove(temp_path)
 
     return output_path
 
 if __name__ == "__main__":
     # Make sure you put the right paths here 
-    input_path = 'testHelloOriginal.mp3'
-    output_path = 'testHelloSlower_1.42.mp3'
+    input_path = 'test1/testHelloOriginal.mp3'
+    output_path = 'test1/testHelloFaster_0.93.mp3'
 
     # The higher the slower, I recommend not going above 1.2. For high values artifacts can appear.
-    slowdown_output = 1.42
+    slowdown_output = 0.93
     slow_down_and_pitch_stretch(input_path, slowdown_output, output_path)
